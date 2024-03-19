11e135c3987600f5366119b71d1f16b83b0c4cec
AgamjotSingh3010 <126556275+AgamjotSingh3010@users.noreply.github.com>
2024-03-19 10:39:25 +0530

    Delete Assignment 2/app.js

--- Assignment 2/app.js
+++
@@ -1,7 +0,0 @@
-
-const url="https://api.giphy.com/v1/gifs/search?api_key=tod4to7L1tkCbruT8wPCgKMxRzDrP58b&q=animals&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
-const giphy=async()=>{
-    let sticker=await fetch(url);
-    const data = await sticker.json();
-    console.log(data);
-};
